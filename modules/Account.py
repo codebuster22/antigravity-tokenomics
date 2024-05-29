28a2b467a946989c4ca08ecbe3be1aa50b48fbfa
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-05-30 01:19:07 +0530

    fix: update the Account module to accept the initial address

--- modules/Account.py
+++ modules/Account.py
@@ -4,8 +4,8 @@ import random
 import string
 
 class Account:
-    def __init__(self):
-        self.address = self._generate_address()
+    def __init__(self, address=None):
+        self.address = address or self._generate_address()
 
     def _generate_address(self):
         return '0x' + ''.join(random.choices(string.hexdigits[:16], k=40))
